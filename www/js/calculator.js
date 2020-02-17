c07c6f6d5789bf264dcdb2eb9fdce71b9a6a081d
Nolan Harman <driazz106@gmail.com>
2020-02-16 22:45:41 -0700

    Better Functionality

--- www/js/calculator.js
+++ www/js/calculator.js
@@ -1,179 +1,103 @@
 var displayData = [];
-var globalMath = [];
-var triggerCheck = [];
+var getFirstValue;
+var getFinalValue;
+var check;
+var answer;
 
-//Create Trigger Value (null);
-var trigger;
-var getNewValue;
-
-const sumReducer = (accumulator, currentValue) => accumulator + currentValue;
-const subReducer = (accumulator, currentValue) => accumulator - currentValue;
-const multReducer = (accumulator, currentValue) => accumulator * currentValue;
-const divReducer = (accumulator, currentValue) => accumulator / currentValue;
-
-
-function clearDisplay() {
-    var checkDisplay = document.getElementById("display").value;
-    if (checkDisplay != "") {
-        displayData = [];
-        globalMath = [];
-        document.getElementById("display").innerHTML = displayData;
-        console.log("Cleared displayData: " + displayData);
-    } else {
-        alert("Screen is empty!");
-    }
-}
 
 function sumNumbers() {
-    getNewValue = document.getElementById("display").value = displayData.join("");
-    var buttonAdd = document.getElementById("buttonJS04").value;
-    console.log("New Value: " + getNewValue + " | globalMath: " + globalMath + " | displayData: " + displayData);
-
+    getFirstValue = document.getElementById("display").value = displayData.join("");
+    check = "+";
+    console.log("First Value: " + getFirstValue);
     if (displayData != "") {
         displayData = [];
-        globalMath.push(parseInt(getNewValue));
-        console.log("Cleared displayData: " + displayData + " | Added to globalMath: " + globalMath);
+        console.log(check);
     }
-
-    if (globalMath != "") {
-        var sumAnswer = globalMath.reduce(sumReducer);
-        console.log("This is the sum: " + sumAnswer); //Getting the sum;
-        document.getElementById("display").innerHTML = sumAnswer;
-        getNewValue = sumAnswer;
-    }
-
-    //Assign trigger to button
-    trigger = buttonAdd;
-    triggerCheck.push(trigger);
-    console.log("trigger array: " + triggerCheck);
 }
 
 function subNumbers() {
-    getNewValue = document.getElementById("display").value = displayData.join("");
-    console.log(getNewValue);
-    var buttonSub = document.getElementById("buttonJS08").value;
-    console.log("New Value: " + getNewValue + " | globalMath: " + globalMath + " | displayData: " + displayData);
-
+    getFirstValue = document.getElementById("display").value = displayData.join("");
+    check = "-";
+    console.log("First Value: " + getFirstValue);
     if (displayData != "") {
         displayData = [];
-        globalMath.push(parseInt(getNewValue));
-        console.log("Cleared displayData: " + displayData + " | Added to globalMath: " + globalMath);
+        console.log(check)
     }
-
-    if (globalMath != "") {
-        var subAnswer = globalMath.reduce(subReducer);
-        console.log("This is the sum: " + subAnswer); //Getting the sum;
-        document.getElementById("display").innerHTML = subAnswer;
-        getNewValue = subAnswer;
-    }
-
-    //Assign trigger to button
-    trigger = buttonSub;
-    triggerCheck.push(trigger);
-    console.log("trigger array: " + triggerCheck);
 }
 
 function multiplyNumbers() {
-    getNewValue = document.getElementById("display").value = displayData.join("");
-    console.log(getNewValue);
-    var buttonMul = document.getElementById("buttonJS12").value;
-    console.log("New Value: " + getNewValue + " | globalMath: " + globalMath + " | displayData: " + displayData);
-
+    getFirstValue = document.getElementById("display").value = displayData.join("");
+    check = "*";
+    console.log("First Value: " + getFirstValue);
     if (displayData != "") {
         displayData = [];
-        globalMath.push(parseInt(getNewValue));
-        console.log("Cleared displayData: " + displayData + " | Added to globalMath: " + globalMath);
+        console.log(check)
     }
-
-    if (globalMath != "") {
-        var multAnswer = globalMath.reduce(multReducer);
-        console.log("This is the sum: " + multAnswer); //Getting the sum;
-        document.getElementById("display").innerHTML = multAnswer;
-        getNewValue = multAnswer;
-    }
-
-    //Assign trigger to button
-    trigger = buttonMul;
-    triggerCheck.push(trigger);
-    console.log("trigger array: " + triggerCheck);
 }
 
 function divideNumbers() {
-    getNewValue = document.getElementById("display").value = displayData.join("");
-    console.log(getNewValue);
-    var buttonDiv = document.getElementById("buttonJS16").value;
-    console.log("New Value: " + getNewValue + " | globalMath: " + globalMath + " | displayData: " + displayData);
-
+    getFirstValue = document.getElementById("display").value = displayData.join("");
+    check = "/";
+    console.log("First Value: " + getFirstValue);
     if (displayData != "") {
         displayData = [];
-        globalMath.push(parseInt(getNewValue));
-        console.log("Cleared displayData: " + displayData + " | Added to globalMath: " + globalMath);
+        console.log(check)
     }
-
-    if (globalMath != "") {
-        var divAnswer = globalMath.reduce(divReducer);
-        console.log("This is the sum: " + divAnswer); //Getting the sum;
-        document.getElementById("display").innerHTML = divAnswer;
-        getNewValue = divAnswer;
-    }
-
-    //Assign trigger to button
-    trigger = buttonDiv;
-    triggerCheck.push(trigger);
-    console.log("trigger array: " + triggerCheck);
 }
 
 function equal() {
-    var getFinalNumber = document.getElementById("display").value = displayData.join("");  //const or let will fix var update;
-    console.log("Final Number: " + getFinalNumber);
+    getFinalValue = document.getElementById("display").value = displayData.join("");
+    console.log(getFinalValue);
 
-    if (getFinalNumber != "") {
-        globalMath.push(parseInt(getFinalNumber));
-        console.log("Pushing last number to globalMath : " + globalMath);
-    } else {
-        alert("Please enter a value!");
-    }
-
-    if (displayData != "") {
+    if (check == "+") {
+        answer = parseInt(getFirstValue) + parseInt(getFinalValue);
+        console.log("Answer: " + answer);
+        document.getElementById("display").innerHTML = answer;
+        check = "";
         displayData = [];
     }
 
-    last = function (array, n) {
-        if (array == null)
-            return void 0;
-        if (n == null)
-            return array[array.length - 1];
-        return array.slice(Math.max(array.length - n, 0));
-    };
-
-    console.log("Last value in triggerArray: " + last(triggerCheck));
-    var check = last(triggerCheck);
-
-    if (check == "+") {
-        var value1 = parseInt(getNewValue);
-        var value2 = parseInt(getFinalNumber);
-        var answer = value1 + value2;
-        document.getElementById("display").innerHTML = answer;
+    if (displayData == "") {
+        displayData.push(answer);
     }
 
     if (check == "-") {
-        var value1 = parseInt(getNewValue);
-        var value2 = parseInt(getFinalNumber);
-        var answer = value1 - value2;
+        answer = parseInt(getFirstValue) - parseInt(getFinalValue);
         document.getElementById("display").innerHTML = answer;
+        console.log("Answer: " + answer);
+        check = "";
+        displayData = [];
+        getFirstValue = answer;
     }
 
-    if (check == "x") {
-        var value1 = parseInt(getNewValue);
-        var value2 = parseInt(getFinalNumber);
-        var answer = value1 * value2;
+    if (displayData == "") {
+        displayData.push(answer);
+    }
+
+    if (check == "*") {
+        answer = parseInt(getFirstValue) * parseInt(getFinalValue);
         document.getElementById("display").innerHTML = answer;
+        console.log("Answer: " + answer);
+        check = "";
+        displayData = [];
+        getFirstValue = answer;
+    }
+
+    if (displayData == "") {
+        displayData.push(answer);
     }
 
     if (check == "/") {
-        var value1 = parseInt(getNewValue);
-        var value2 = parseInt(getFinalNumber);
-        var answer = value1 / value2;
+        answer = parseInt(getFirstValue) / parseInt(getFinalValue);
         document.getElementById("display").innerHTML = answer;
+        console.log("Answer: " + answer);
+        check = "";
+        displayData = [];
+        getFirstValue = answer;
+    }
+
+    if (displayData == "") {
+        displayData.push(answer);
     }
 }
+
